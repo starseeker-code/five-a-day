02a4bbee5542c54798a39d6b83336cb9315d46d4
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2025-08-27 20:56:43 +0200

    Created admin

--- project/core/admin.py
+++ project/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(ExpenseCategory)
+admin.site.register(Expense)
+admin.site.register(RecurringExpenseTemplate)
+admin.site.register(FinancialPeriod)
+admin.site.register(EnrollmentType)
+admin.site.register(Enrollment)
+admin.site.register(Payment)
+admin.site.register(Payroll)
+admin.site.register(Teacher)
+admin.site.register(Group)
+admin.site.register(Student)
+admin.site.register(Parent)
+admin.site.register(StudentParent)
